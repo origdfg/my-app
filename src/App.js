7c3b292cc14c1721cfcec34b1313e74f1dbae2e9
origdfg <5B0G0041@stust.edu.tw>
2024-05-02 11:16:48 +0800

    1251

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import MySlider from "./MySlider";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>5B0G0041趙偉聖</h1>
+      <h2>第一個元件</h2>
+      <span>R: </span><MySlider />
+      <span>G: </span><MySlider />
+      <span>B: </span><MySlider />
+      {/* <header className="App-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello React! 2024.5.1</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
